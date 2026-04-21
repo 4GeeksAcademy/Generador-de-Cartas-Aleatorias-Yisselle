3072ebb71d7891bfa1b1d5d4304871e8f9a47418
NavarreteYisselle <yiselle.navarrete21@gmail.com>
2026-04-21 23:02:29 +0000

    card project

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let suits = ["heart", "diamond", "spade", "club"];
+  let suitSymbols = {
+    heart: "♥",
+    diamond: "♦",
+    spade: "♠",
+    club: "♣",
+  };
+
+
+  let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
+  let randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  let randomValue = values[Math.floor(Math.random() * values.length)];
+
+  let card = document.createElement("div");
+  card.classList.add("card");
+
+  if (randomSuit === "heart" || randomSuit === "diamond") {
+    card.style.color = "red";
+  } else {
+    card.style.color = "black";
+  }
+
+  card.innerHTML = `
+<div class="top">${suitSymbols[randomSuit]}</div>
+<div class="number">${randomValue}</div>
+<div class="bottom">${suitSymbols[randomSuit]}</div>
+`;
+
+  document.querySelector(".container-fluid").appendChild(card);
 };
